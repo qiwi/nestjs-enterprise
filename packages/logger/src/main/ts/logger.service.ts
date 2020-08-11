86e74e59ffdcd6c2febd09b44ca3ce3cd9246d0c
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2020-08-11 18:41:00 +0300

    refactor(logger/service): migrate to app.pipe & logger.pipe

--- packages/logger/src/main/ts/logger.service.ts
+++ packages/logger/src/main/ts/logger.service.ts
@@ -6,61 +6,43 @@ import {
 } from '@nestjs/common'
 import createWinstonLogger from './winston'
 import { ILogger, IConfig, LogLevel } from '@qiwi/substrate'
-import { Logwrap, mdc, ILogEntry } from '@qiwi/logwrap'
+import { Logwrap, mdc } from '@qiwi/logwrap'
 // @ts-ignore
 import { DEFAULT_NS } from '@qiwi/mware-context'
+import { createAppPipe } from './app.pipe';
+import { createLoggerPipe } from './logger.pipe';
 
 @Injectable()
 export class LoggerService extends Logwrap
   implements ILogger, LoggerServiceNest {
+  // @ts-ignore
   constructor(
     @Inject('ILoggerPipeline') pipeline: any[],
     @Inject('IConfigService') config: IConfig,
   ) {
-    const host = os.hostname()
-    const name = config.get('name')
-    const version = config.get('version')
     const loggerConfig = config.get('logger')
-    const winstonLogger = createWinstonLogger(loggerConfig)
-
-    // todo: в pipe
-    const appInfo = (entry: ILogEntry) => {
-      Object.assign(entry.meta, {
-        name,
-        version,
-        host,
-      })
-      return entry
-    }
-
-    const logger = (entry: ILogEntry) => {
-      const { input, meta, level } = entry
-      winstonLogger.log({
-        level,
-        message: input.join(' '),
-        meta,
-      })
-
-      return entry
-    }
-
     super({
       level: loggerConfig.level,
-      pipeline: [mdc({ ns: DEFAULT_NS }), appInfo, ...pipeline, logger],
+      pipeline: [
+        mdc({ ns: DEFAULT_NS }),
+        createAppPipe(config.get('name'), config.get('version'), os.hostname()),
+        ...pipeline,
+        createLoggerPipe(createWinstonLogger(loggerConfig))
+      ],
     })
+  }
 
-    this.log = (...args): void => {
-      return this.info(...args)
-    }
+  log = (...args: any[]): void => {
+    return this.info(...args)
+  }
 
-    this.push = (entry: {
-      meta: Record<string, any>
-      level: LogLevel
-      input: any[]
-    }) => {
-      // @ts-ignore
-      LoggerService.perform(this.level, this.pipeline, entry)
-    }
+  push = (entry: {
+    meta: Record<string, any>
+    level: LogLevel
+    input: any[]
+  }) => {
+    // @ts-ignore
+    LoggerService.perform(this.level, this.pipeline, entry)
   }
 
   verbose(...args: any[]): void {
