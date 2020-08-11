edb248ece76fd5a17bf53f4bcf81b3402bb21cce
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2020-08-11 18:40:29 +0300

    feat(logger/logger.pipe): new pipe

---
+++ packages/logger/src/main/ts/logger.pipe.ts
@@ -0,0 +1,14 @@
+import { ILogEntry } from '@qiwi/logwrap';
+import { ILogger } from '@qiwi/substrate';
+
+export const createLoggerPipe = (logger: ILogger) =>
+  (entry: ILogEntry): ILogEntry => {
+    const { input, meta, level } = entry
+    logger.log({
+      level,
+      message: input.join(' '),
+      meta,
+    })
+
+    return entry
+  }
