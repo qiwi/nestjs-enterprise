676ef6ad37e536564bf9b116560d727b237cf8e7
Anton Golub <antongolub@antongolub.com>
2022-08-16 10:13:36 +0300

    fix: use async factory as ConfigService provider

--- packages/config/src/main/ts/config.module.ts
+++ packages/config/src/main/ts/config.module.ts
@@ -5,7 +5,11 @@ import { ConfigService } from './config.service'
 @Global()
 @Module({
   controllers: [],
-  providers: [{ provide: 'IConfigService', useClass: ConfigService }],
+  providers: [{ provide: 'IConfigService', useFactory: async (options: Record<any, any> = {}) => {
+    const service = new ConfigService(options.path || options.config)
+    await service.ready
+    return service
+  }}],
   exports: ['IConfigService'],
 })
 export class ConfigModule {
--- packages/mdc/src/main/ts/index.ts
+++ packages/mdc/src/main/ts/index.ts
@@ -1,6 +1,5 @@
 // @ts-ignore
-export { mdc, validator, logger } from '@qiwi/mware'
+export { mdc, validator, logger, context as clscxt } from '@qiwi/mware'
 export { MdcModule } from './mdc.module'
 export { MdcService, IMdcService } from './mdc.service'
-// @ts-ignore
-export { default as clscxt } from '@qiwi/mware-context'
+
