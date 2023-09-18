6c2e74be851817d49672ad8ad21b215c23388dea
Choro Abdymanapov <oljekechoro@mail.ru>
2023-09-18 20:57:56 +0600

    feat(config): use cwd instead of root, support environment configs (#94)
    BREAKING CHANGE: config is now resolved by cwd

--- packages/config/src/main/ts/config.service.ts
+++ packages/config/src/main/ts/config.service.ts
@@ -4,46 +4,35 @@ import {
   IConfig as IConfigService,
 } from '@qiwi/uniconfig'
 
-export const DEFAULT_LOCAL_CONFIG_PATH = '<root>/config/local.json'
-export const DEFAULT_KUBE_CONFIG_PATH = '<root>/config/kube.json'
+export const DEFAULT_LOCAL_CONFIG_PATH = '<cwd>/config/local.json'
+export const DEFAULT_KUBE_CONFIG_PATH = '<cwd>/config/kube.json'
 
-export const resolveConfigPath = (path?: string, local?: boolean): string => {
+export const resolveConfigPath = (path?: string, local?: boolean, env?: string): string | string[][] => {
   if (path) {
     return path
   }
 
-  return local ? DEFAULT_LOCAL_CONFIG_PATH : DEFAULT_KUBE_CONFIG_PATH
+  return local
+    ? DEFAULT_LOCAL_CONFIG_PATH
+    : env
+      ? [
+        [`<cwd>/config/${env}.json`],
+        [DEFAULT_KUBE_CONFIG_PATH],
+      ]
+      : DEFAULT_KUBE_CONFIG_PATH
 }
 
 const normalizeOpts = (opts?: string | Record<any, any>) => {
   if (typeof opts === 'string' || opts === undefined) {
     return {
       mode: ASYNC,
-      data: {
-        data: {
-          data: {
-            target: '$env:ENVIRONMENT_PROFILE_NAME',
-            local: '$env:LOCAL',
-            path: opts,
-            resolveConfigPath,
-          },
-          template: `{{=it.resolveConfigPath(it.path, it.local)}}`,
-        },
-        sources: {
-          env: {
-            pipeline: 'env',
-          },
-        },
-      },
-      pipeline: 'datatree>dot>path>file>json>datatree',
+      data: resolveConfigPath(opts, !!process.env.LOCAL, process.env.ENVIRONMENT_PROFILE_NAME),
+      pipeline: 'path>file>json>datatree',
     }
   }
 
   return opts
 }
-
-
-
 
 export class ConfigService extends Config implements IConfigService {
   constructor(opts?: string | Record<any, any>) {
@@ -51,4 +40,4 @@ export class ConfigService extends Config implements IConfigService {
   }
 }
 
-export type {IConfig as IConfigService} from '@qiwi/uniconfig'
+export type { IConfig as IConfigService } from '@qiwi/uniconfig'
