216f1e18c0eab702db99da535d7768486007415c
Choro Abdymanapov <c.abdymanapov@qiwi.com>
2023-10-12 19:07:29 +0600

    fix: export config service factory

--- packages/config/src/main/ts/index.ts
+++ packages/config/src/main/ts/index.ts
@@ -4,5 +4,6 @@ export {
   DEFAULT_KUBE_CONFIG_PATH,
   DEFAULT_LOCAL_CONFIG_PATH,
   ConfigService,
+  configServiceFactory,
 } from './config.service'
 export type { IConfigService } from './config.service'
